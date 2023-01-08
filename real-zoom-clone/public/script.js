116a12de9b58412e0f9fb4b598666aead6f4c86d
zombie22 <fatihkabbani223@gmail.com>
2023-01-08 03:31:36 +0100

    worked

--- real-zoom-clone/public/script.js
+++ real-zoom-clone/public/script.js
@@ -1,28 +1,56 @@
 const socket = io('/');
-const vid = document.getElementById('video-grid');  
+const vid = document.getElementById('video-grid');
 const mypeer = new Peer(undefined, {
     host: '/',
     port: '3001'
+
 });
 const myvid = document.createElement('video');
 myvid.muted = true;
-
+const peers = {};
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
-    addvidstream(myvid , stream)
+    addvidstream(myvid, stream)
+    mypeer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addvidstream(video, userVideoStream);
+        })
+    })
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+
+    })
 })
-mypeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id)
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) peers[userId].close();
 
 })
-socket.on('user-connected' , userId => {
+
+mypeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+socket.on('user-connected', userId => {
     console.log('user-connected' + userId)
 })
-function addvidstream(video , stream){
+function connectToNewUser(userId, stream) {
+    const call = mypeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addvidstream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove();
+    })
+    peers[userId] = call;
+}
+function addvidstream(video, stream) {
     video.srcObject = stream;
-    video.addEventListener('loadedmetadata' , () =>{
+    video.addEventListener('loadedmetadata', () => {
         video.play()
     });
     vid.append(video);
